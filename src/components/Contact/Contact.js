caff7874bf1b2c6e4a7d54996264fbf1771c4f0f
Jade Rubick <jade@gremlin.com>
2020-09-23 07:27:11 -0700

    Add hacky link to LinkedIn

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -3,6 +3,7 @@
 import { navigate } from "gatsby";
 import React from "react";
 import { StaticQuery, graphql } from "gatsby";
+import { FaGithub, FaStackOverflow, FaLinkedin } from 'react-icons/fa'
 
 import theme from "../../theme/theme.yaml";
 
@@ -10,7 +11,9 @@ class Contact extends React.Component {
 
   render() {
     return (
-      <p>I can be reached at jade at rubick dot com.
+      <p>I can be reached at jade at rubick dot com.<br />
+
+      And on LinkedIn: <a href="https://www.linkedin.com/in/jaderubick/" target="_blank"><FaLinkedin/></a>
               <re-icons></re-icons>
       </p>
 
