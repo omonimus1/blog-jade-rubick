ab34f2d6838521a0589e7238510b6342361a1962
Gaspard Beernaert <gaspard@gmail.com>
2020-10-12 22:48:14 +0200

    fix: typo in stackexchange link
    http://www.plantworking.com is really interesting, but are you still in the depilation cream business?

--- content/meta/config.js
+++ content/meta/config.js
@@ -11,7 +11,7 @@ module.exports = {
   authorName: "Jade Rubick",
   authorTwitterAccount: "@jaderubick",
   authorGithub: "https://github.com/jadeforrest",
-  authorStackoverflow: "https://stackexchttps://stackexchange.com/users/35586/",
+  authorStackoverflow: "https://stackexchange.com/users/35586/",
   authorLinkedin: "https://www.linkedin.com/in/jaderubick/",
 
   /* header text */
